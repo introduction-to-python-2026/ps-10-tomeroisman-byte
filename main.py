6e79e87a6f9c2e4833ead090d6043c08574fa0fe
sleepy_tomer <tomeroisman@gmail.com>
2026-01-13 10:36:57 +0200

    Add edge detection and visualization with matplotlib

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-
+import matplotlib.pyplot as plt
+from image_utils import detect_edges
+original_gray, edge_result = detect_edges('whatsapp_image.jpg', threshold=50)
+plt.figure(figsize=(10, 5))
+plt.imshow(edge_result, cmap='gray')
+plt.title("White Edges")
+plt.axis('off')
+plt.show()
